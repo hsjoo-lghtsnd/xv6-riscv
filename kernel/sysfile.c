c43dfc2c725636b629ffcee3d27ed00e7d842d06
hsjoo-lghtsnd <zxcqa123@gmail.com>
2025-07-31 00:10:06 +0900

    H1: _Atomic uint64..

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -16,9 +16,7 @@
 #include "file.h"
 #include "fcntl.h"
 
-#include <stdatomic.h>
-
-static atomic_ulong read_count=0;
+static _Atomic uint64 read_count=0;
 
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
